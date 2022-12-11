d54eae25d969e7612c2908a972d232f67e88553a
Lixxxo <luis.rojas04@alumnos.ucn.cl>
2022-12-10 23:37:12 -0300

    feat: add models (Contract, Employee, Project)

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from routes.employee import employees
+from routes.contract import contracts
+from routes.format import format
+
+from flask_sqlalchemy import SQLAlchemy
+from config import SQLALCHEMY_DATABASE_URI
+from models.models import Employee
+
+
+app = Flask(__name__)
+
+# settings
+app.secret_key = 'mysecret'
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# no cache
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+SQLAlchemy(app)
+
+app.register_blueprint(employees)
+app.register_blueprint(contracts)
+app.register_blueprint(format)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', employees=Employee.query.all())
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+@app.context_processor
+def utility_processor():
+    from utils.format import format_currency
+    return dict(format_currency=format_currency)
---
+++ models/models.py
@@ -0,0 +1,86 @@
+from utils.db import db
+from datetime import date
+
+
+class Contract(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    start_date = db.Column(db.Date, default=date.today)
+    end_date = db.Column(db.Date, nullable=True)
+
+    state = db.Column(db.String(10), default="active")
+    salary = db.Column(db.String(250), nullable=False)
+
+    employee_id = db.Column(db.Integer, db.ForeignKey("employee.id"))
+    project_id = db.Column(db.Integer, db.ForeignKey("project.id"))
+
+    def __init__(self, salary, employee_id, project_id):
+        self.salary = salary
+        self.employee_id = employee_id
+        self.project_id = project_id
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'start_date': str(self.start_date),
+            'end_date': str(self.end_date),
+            'salary': self.salary,
+            'state': self.state,
+            'employee_id': self.employee_id,
+            'project_id': self.project_id,
+            'project': self.project
+        }
+
+    @property
+    def project(self):
+        project = Project.query.get(self.project_id)
+        return project.to_json()
+
+
+class Employee(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rut = db.Column(db.String(250))
+    name = db.Column(db.String(250))
+
+    contracts = db.relationship("Contract")
+
+    def __init__(self, rut, name):
+        self.rut = rut
+        self.name = name
+
+    def to_json(self):
+        contracts = [c.to_json() for c in self.contracts]
+        return {
+            'id': self.id,
+            'rut': self.rut,
+            'name': self.name,
+            'contracts': contracts,
+            'total_salary': self.total_salary,
+            'active_contracts': self.active_contracts
+        }
+
+    @property
+    def total_salary(self):
+        return sum([int(c["salary"]) for c in self.active_contracts])
+
+    @property
+    def active_contracts(self):
+        return [c.to_json() for c in self.contracts if c.state == "active"]
+
+
+class Project(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(250), unique=True)
+
+    contracts = db.relationship("Contract")
+
+    def __init__(self, name):
+        self.name = name
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
