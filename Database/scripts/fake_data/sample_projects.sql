448cb354a218b748328859d7f06342bae323c51d
Lixxxo <luis.rojas04@alumnos.ucn.cl>
2022-12-10 23:48:10 -0300

    docs: create sql fake data script

---
+++ Database/scripts/fake_data/sample_projects.sql
@@ -0,0 +1,9 @@
+delete from project where id > 0;
+
+insert into project values (1, 'Proyecto BA');
+insert into project values (2, 'Proyecto 64');
+insert into project values (3, 'Proyecto #');
+insert into project values (4, 'Proyecto Nuevo Horizonte');
+insert into project values (5, 'Proyecto @@');
+insert into project values (7, 'Proyecto 21');
+insert into project values (8, 'Proyecto GH55');
